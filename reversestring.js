89de0e1c0fdce4b4e71cd9c52aefbb7f97e44314
ahmedfarook <ahmedgaziyani@gmail.com>
2019-09-08 13:26:32 +0530

    Delete reversestring.js

--- reversestring.js
+++
@@ -1,9 +0,0 @@
-function namereverse(name) {
-    count = name.length
-    var reverse = "";
-    for (i=0; i < name.length; i++){
-        reverse += (name[count -= 1])
-     }
-     return reverse
-}
-console.log(namereverse("Argument goes here"))
